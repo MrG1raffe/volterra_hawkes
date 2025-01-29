ae5e090dbb6252b40d8aae2ac8c551146893920d
Dimitri Sotnikov <dmitrii.sotnikov@engie.com>
2025-01-29 19:40:52 +0100

    Init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='volterra-hawkes',
+   version='1.0',
+   description='Volterra process simulation and solvers for Volterra equations',
+   author='MrG1raffe',
+   author_email='dimitri.sotnikov@gmail.com',
+   packages=find_packages(),
+   install_requires=['numpy', 'matplotlib', 'scipy'], #external packages as dependencies
+)
